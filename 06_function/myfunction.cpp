fe84efa3b84a0afe6f4ed80d1e8addb9c1409dc2
utkarshsd <utkarshsushantpandey@gmail.com>
2025-07-29 06:45:21 +0000

    loop

--- 06_function/myfunction.cpp
+++ 06_function/myfunction.cpp
@@ -4,13 +4,30 @@
 //void
 
 #include <iostream>
+using namespace std;
 
 int checkTemperature(int temperature){
     return temperature;
 
 }
+//declaration of function 
+void serveChai(int cups);
+void makeChai(){
+    cout<<"boiling water, adding tea leaves, straining...";
+}
+
+void serveChai(){}
 
 int main(){
     int temp = checkTemperature(50);
-    std::cout<< temp;
+    cout<< temp;
+    makeChai();//calling a function
+
+    serveChai(3);
+    return 0;
+
+}
+
+void serveChai(int cups){
+    cout << " serving " << cups<< " of chai";
 }
